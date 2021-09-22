6494ddbd13f0a804d2945d9f6bf3dad9888eb51a
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-09-22 13:28:25 -0500

    wip: add js file

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# Cocktail recipe finder
-
-This is my first project for GA SEIR Program
+# BoozeHound Cocktail Finder
+This is my first project for GA SEIR Program.
 
 # Technologies Used
 
 -HTML
--CSS
+-CSS/Skeleton
 -JavaScript/jQuery
+
 
 # Screenshots
 
@@ -14,7 +14,8 @@ This is my first project for GA SEIR Program
 [Click here](#) to see the deployed project
 
 # Future Enhancements 
+I'd like to add a hamburger menu with options for different functions, such as cocktails by spirit, by name, by ingredients, by multiple ingredients. 
 
 
 
-
+ 
--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 //IPO - Input -> Process -> Output
-
+let cocktailData
 
 // Variable and Constants
 const BASE_URL = `https://www.thecocktaildb.com/api/json/v2/`;
 const API_KEY = '9973533';
-// Cached Element References
 
+const $instructions = $('#instructions')
+const $measurements = $('#measurements')
+const $ingredients = $('#ingredients')
+const $name = $('#name');
+const $sniff = $('.sniff');
 
 // Event Listeners
-
+$sniff.on('click', getData);
 
 // Functions
 function getData() {
@@ -16,13 +20,30 @@ function getData() {
     $.ajax(`${BASE_URL}${API_KEY}/random.php`)
     .then(function(data) {
         // save the data locally
-        // transfer the data to the DOM
         console.log(data);
+        cocktailData = data;
+        // transfer the data to the DOM
+        render(cocktailData.drinks[0]);
     }, function(error) {
         console.log(error);
     });
     
 }
+
+function render(cocktail) {
+    
+    console.log(cocktail)    
+$name.text(cocktail.strDrink)
+$ingredients.text(`${cocktail.strIngredient1}, ${cocktail.strIngredient2} , ${cocktail.strIngredient3}, ${cocktail.strIngredient4}, ${cocktail.strIngredient5}, ${cocktail.strIngredient6}, ${cocktail.strIngredient7}, ${cocktail.strIngredient8}, ${cocktail.strIngredient9}`)
+$measurements.text(`${cocktail.strMeasure1}, ${cocktail.strMeasure2}, ${cocktail.strMeasure3}, ${cocktail.strMeasure4}, ${cocktail.strMeasure5}, ${cocktail.strMeasure6}, ${cocktail.strMeasure7} ,${cocktail.strMeasure8}, ${cocktail.strMeasure9}`)
+$instructions.text(cocktail.strInstructions)
+                
+   $('img').attr('src', cocktail.strDrinkThumb)
+
+    // $('main').html(html);
+}
+
+        
         
 
 
